0f498f98394566e17ba36adeb3cccb4002ba79a6
Raluca Ioana Stinga <raluca.stinga@student.manchester.ac.uk>
2021-02-23 13:02:49 +0000

    Add data to database

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Component
 @Profile({ "default", "test" })
@@ -32,6 +33,16 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
 		}
 
 		// Build and save initial models here.
-
+		Venue v1 = new Venue();
+		v1.setId(1);
+		v1.setName("Kilburn G23");
+		v1.setCapacity(80);
+		venueService.save(v1);
+		
+		Venue v2 = new Venue();
+		v2.setId(2);
+		v2.setName("Online");
+		v2.setCapacity(100000);
+		venueService.save(v2);
 	}
 }
