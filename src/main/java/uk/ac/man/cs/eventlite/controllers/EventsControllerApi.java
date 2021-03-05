98b3d25209503f698124bb8f1556fde47e28de7d
Joshua Mather <joshua.mather@student.manchester.ac.uk>
2021-03-05 11:45:16 +0000

    Implement new roles and related security
    A new organiser role has been added and certain actions, such as add new
    event and delete event, require this role to carry out. Additionally a
    delete issue has been fixed and the add new event page updated.

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApi.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApi.java
@@ -72,7 +72,7 @@ public class EventsControllerApi {
 		return ResponseEntity.created(location).build();
 	}
 	
-	@DeleteMapping("/delete/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteEventById(@PathVariable Long id) {
 		eventService.deleteById(id);
 	}
