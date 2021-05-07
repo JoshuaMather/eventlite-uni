44effd00141d92f8b7baa254c5bfa535b0b21ce6
ziyang.yu <ziyang.yu@student.manchester.ac.uk>
2021-05-07 01:30:27 +0100

    Integration tests for post mapping - delete

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerIntegrationTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerIntegrationTest.java
@@ -11,12 +11,22 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.StringEndsWith.endsWith;
 
 import uk.ac.man.cs.eventlite.EventLite;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,11 +36,17 @@ public class EventsControllerIntegrationTest extends AbstractTransactionalJUnit4
 
 	@LocalServerPort
 	private int port;
-
+    
 	private WebTestClient client;
-
+    
+	private int rows;
+	private static Pattern CSRF = Pattern.compile("(?s).*name=\"_csrf\".*?value=\"([^\"]+).*");
+	private static String SESSION_KEY = "JSESSIONID";
+	private static String CSRF_HEADER = "X-CSRF-TOKEN";
+	
 	@BeforeEach
 	public void setup() {
+        rows = countRowsInTable("event");
 		client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
 	}
 
@@ -48,6 +64,48 @@ public class EventsControllerIntegrationTest extends AbstractTransactionalJUnit4
 		.consumeWith(result -> {
 			assertThat(result.getResponseBody(), containsString("COMP23412 Showcase, group F"));
 		});
+	}
+	
+	@Test
+	public void deleteEventNoUser() {
+		
+		client.delete().uri("/events/8").accept(MediaType.TEXT_HTML).exchange().expectStatus().isFound()
+				.expectHeader().value("Location", endsWith("/sign-in"));
+
+		assertThat(rows, equalTo(countRowsInTable("event")));
+	}
+
+	@Test
+	@DirtiesContext
+	public void deleteEventWithUser() {
+		String[] tokens = login();
+
+		client.delete().uri("/events/8").accept(MediaType.TEXT_HTML).header(CSRF_HEADER, tokens[0])
+				.cookie(SESSION_KEY, tokens[1]).exchange().expectStatus().isFound().expectHeader()
+				.value("Location", endsWith("/events"));
+
+		assertThat(rows - 1, equalTo(countRowsInTable("event")));
+	}
+	
+	private String[] login() {
+		String[] tokens = new String[2];
+
+		EntityExchangeResult<String> result = client.mutate()
+				.filter(basicAuthentication("Mustafa", "Mustafa")).build().get()
+				.uri("/").accept(MediaType.TEXT_HTML).exchange().expectBody(String.class).returnResult();
+		tokens[0] = csrfToken(result.getResponseBody());
+		
+		tokens[1] = result.getResponseCookies().getFirst(SESSION_KEY).getValue();
+
+		return tokens;
+	}
+	
+	private String csrfToken(String body) {
+		Matcher matcher = CSRF.matcher(body);
+		
+		assertThat(matcher.matches(), equalTo(true));
+
+		return matcher.group(1);
 	}
 	
 }
--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerIntegrationTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package uk.ac.man.cs.eventlite.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,12 +12,19 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import uk.ac.man.cs.eventlite.EventLite;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.StringEndsWith.endsWith;
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,11 +34,18 @@ public class VenuesControllerIntegrationTest extends AbstractTransactionalJUnit4
 
 	@LocalServerPort
 	private int port;
+	
+	private int row;
 
 	private WebTestClient client;
+	
+	private static Pattern CSRF = Pattern.compile("(?s).*name=\"_csrf\".*?value=\"([^\"]+).*");
+	private static String SESSION_KEY = "JSESSIONID";
+	private static String CSRF_HEADER = "X-CSRF-TOKEN";
 
 	@BeforeEach
 	public void setup() {
+		row = countRowsInTable("venue");
 		client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
 	}
 
@@ -39,4 +54,33 @@ public class VenuesControllerIntegrationTest extends AbstractTransactionalJUnit4
 		client.get().uri("/venues").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk();
 	}
 	
+	@Test
+	public void deleteVenueNoUser() {
+		
+		client.delete().uri("/venues/6").accept(MediaType.TEXT_HTML).exchange().expectStatus().isFound()
+				.expectHeader().value("Location", endsWith("/sign-in"));
+
+		assertThat(row, equalTo(countRowsInTable("venue")));
+	}
+	
+	
+	private String[] login() {
+		String[] tokens = new String[2];
+
+		EntityExchangeResult<String> result = client.mutate()
+				.filter(basicAuthentication("Mustafa", "Mustafa")).build().get()
+				.uri("/").accept(MediaType.TEXT_HTML).exchange().expectBody(String.class).returnResult();
+		tokens[0] = getCsrfToken(result.getResponseBody());
+		tokens[1] = result.getResponseCookies().getFirst(SESSION_KEY).getValue();
+
+		return tokens;
+	}
+	
+	private String getCsrfToken(String body) {
+		Matcher matcher = CSRF.matcher(body);
+
+		assertThat(matcher.matches(), equalTo(true));
+
+		return matcher.group(1);
+	}
 }
